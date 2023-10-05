dd8c5f654df6a4bde3b805af7920637942194529
NUTTAKITCS <nuttakit.t@kkumail.com>
2023-10-05 18:25:42 +0700

    initial commit

---
+++ Testdigi.java
@@ -0,0 +1,16 @@
+package test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Testdigi {
+
+	private SumOfDigits sumofdigits; 
+
+	@Test
+    public void testSumOfDigitsWithPositiveNumber() {
+        int result = SumOfDigits.sumOfDigits(190);
+        assertEquals(10, result);
+    }
+
+}
